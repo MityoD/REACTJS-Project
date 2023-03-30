8b50a3f383626552643604c65babb9012a42d04d
MityoD <mityo91@gmail.com>
2023-03-30 22:14:21 +0300

    import removed

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,14 +1,15 @@
 import Figure from 'react-bootstrap/Figure';
-
+import { Button } from 'react-bootstrap';
 export const Projects = () => {
+  
     return (
         <div >
-            <Figure style={{ marginLeft:'50%', transform:'translate(-50%)'}}  >
+            <Figure style={{ marginLeft: '50%', transform: 'translate(-50%)' }}  >
                 <Figure.Caption>
                     Project page is under development...
                 </Figure.Caption>
-                <hr/>
-                <Figure.Image style={{borderRadius:'25px', overflow:'hidden'}}
+                <hr />
+                <Figure.Image style={{ borderRadius: '25px', overflow: 'hidden' }}
                     width={630}
                     height={390}
                     alt="171x1z0"
